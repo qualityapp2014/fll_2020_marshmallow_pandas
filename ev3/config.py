d019cc584d785c2390f0fab43c7d8aae7dccc3cf
jennifer-m-gao <jennifer.m.gao@gmail.com>
2020-11-16 17:09:28 -0800

    update config and add m02/m11

--- ev3/config.py
+++ ev3/config.py
@@ -2,17 +2,24 @@
 from core import *
 
 
-# Create your objects here.
-ev3 = EV3Brick()
-ev3.speaker.set_speech_options('en', 'm1', 160, None)
+# Create your objects here. 
+ev3 = EV3Brick() 
+ev3.speaker.set_speech_options('en', 'f1', 160, None) 
 
 
-# Configure the robot and sensors
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.A)
-robot = DriveBase(left_motor, right_motor, 80, 98)
+# Configure drive motors
+motor_left = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+motor_right = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+robot = DriveBase(motor_left, motor_right, 87, 111)
 robot.settings(400, 100, 90, 45)
-gyro = GyroSensor(Port.S1, Direction.COUNTERCLOCKWISE)
-color_top = ColorSensor(Port.S2)
-color_left = ColorSensor(Port.S3)
+
+# Configure medium motors
+motor_med_left = Motor(Port.B)
+motor_med_right = Motor(Port.C)
+
+#Configure the sensors
+color_left = ColorSensor(Port.S1)
 color_right = ColorSensor(Port.S4)
+
+ultrasonic = UltrasonicSensor(Port.S2)
+gyro = GyroSensor(Port.S3, Direction.COUNTERCLOCKWISE)
---
+++ ev3/m02_step_counter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env pybricks-micropython
+
+#Importing configurations
+from config import *
+
+#Moving to the mission
+robot.straight(-850)
+
+robot.stop()
+robot.settings(20, 10, 90, 45)
+
+robot.straight(-190)
+
+robot.stop()
+robot.settings(400, 100, 90, 45)
+
+#Going back from the mission
+robot.straight(1000)
