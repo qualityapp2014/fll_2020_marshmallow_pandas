443630226af5e2dfae2c02e23f62b14a07493919
elainecchen <45884935+elainecchen@users.noreply.github.com>
2021-03-13 22:17:32 -0800

    Run 02 (#23)

---
+++ ev3/combo/run_02.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env pybricks-micropython
+from robot import *
+
+
+def run():
+    r = Robot()
+    gyro.reset_angle(0)
+
+    r.move(350, 100)
+    r.move(100, 850)
+    robot.drive(0, 0)
+    wait(500)
+    robot.stop()
+    r.move(-200, 90)
+    r.move(-300, 700)
+    r.stop()
+
+run()
