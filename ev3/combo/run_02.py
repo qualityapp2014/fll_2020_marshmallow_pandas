46217f5952894eb332fe1b3ec9c3787aef77a524
Jennifer Gao <jennifer.m.gao@gmail.com>
2021-03-14 19:41:44 -0800

    find tune run 3

--- ev3/combo/run_02.py
+++ ev3/combo/run_02.py
@@ -2,6 +2,10 @@
 from robot import *
 
 
+def is_right_white():
+    return is_white(color_right.reflection())
+
+
 def run():
     r = Robot()
     gyro.reset_angle(0)
@@ -35,22 +39,25 @@ def run():
     r.move(40, 30, stop=True)
     
     # Lift crate
-    motor_med_left.run_angle(1000, -1140)
-    motor_med_left.run_angle(1000, 1140, wait=False)
+    motor_med_left.run_angle(1000, -1160)
+    motor_med_left.run_angle(1000, 1160, wait=False)
     wait(300)
 
     # Back off and set arm for health unit
-    r.move(-100, 100)
+    r.move(-120, 100)
     motor_med_right.run_angle(1000, 3700, wait=False)
 
-    # Pick up health unit
-    r.turn(-136, -135, 50)
+    # Find lane by checking on white color
+    r.turn(-130, -100, 50)
+    r.turn(-150, 0, 20, terminate=is_right_white)
+
+    # Follow the line to pick up the health unit
     r.follow(280, 50, use_left=False)
 
     # Return to the base
     motor_med_right.run_angle(1000, -3700, wait=False)
-    r.move(-70, 200)
-    r.turn(-25, -100, 50)
+    r.move(-50, 200)
+    r.turn(-25, -100, 60)
     r.move(-560, 400, stop=True)
     motor_med_right.run_angle(1000, 4100, wait=False)
 
