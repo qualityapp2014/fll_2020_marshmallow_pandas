f85d936ea9c68105c6970565903fddf467e2b396
jennifer-m-gao <46030494+jennifer-m-gao@users.noreply.github.com>
2021-03-17 22:39:50 -0700

    refresh code after tuning (#30)

--- ev3/combo/run_01.py
+++ ev3/combo/run_01.py
@@ -8,20 +8,20 @@ def run():
     # Turn and move to the bench
     # Attachment will trigger and pick up
     r.turn(-20, 100, 20)
-    r.turn(5, 100, 20)
+    r.turn(10, 100, 20)
     r.move(70, 100)
-    r.move(25, 30, stop=True)
+    r.move(30, 50, stop=True)
 
     # Move back and turn 180 to drop the cubes
-    r.move(-120, 100)
-    r.turn(176, 15, 60)
-    r.move(-210,100)
-    r.move(-30, 20)
+    r.move(-80, 100)
+    r.turn(186, 15, 40)
+    r.move(-170, 100)
+    r.move(-20, 50)
 
     # Move back to the base
     r.move(200, 200)
-    r.turn(180, 200, 30)
-    r.move(60, 50)
+    r.turn(180, 160, 30)
+    r.move(30, 50)
     r.stop()
 
 if __name__ == "__main__":
